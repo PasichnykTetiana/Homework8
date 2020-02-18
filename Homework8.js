0d2fb9254cd66277764c481d8cc8b0b1362e103b
Tetiana <t.pasichnyk@ukr.net>
2020-02-19 00:23:05 +0200

    first js

---
+++ Homework8.js
@@ -0,0 +1,25 @@
+//1
+// var i=0;
+// while (i<100) {
+//     i += 1;
+//     console.log(i)
+// }
+//2
+// var i;
+// for (i=11; i<=33; i+=1){
+//     console.log(i)
+// }
+//3
+// var i=0;
+// while (i<100) {
+//     i+=1; {
+//         if(i % 2 ===0){
+//             console.log(i);
+//         }
+//     }
+// }
+// var i, sum=0;
+// for (i=0; i<100; i+=1){
+//     sum=sum+i;}
+// console.log(sum);
+
