cc0af868dc2b6b02040c55d6a4c85b897ac72509
Tetiana <t.pasichnyk@ukr.net>
2020-02-20 00:32:33 +0200

    2

--- Homework8.js
+++ Homework8.js
@@ -18,8 +18,15 @@
 //         }
 //     }
 // }
+//4
 // var i, sum=0;
 // for (i=0; i<100; i+=1){
 //     sum=sum+i;}
 // console.log(sum);
-
+//5
+// var n=1000, num=0;
+// while (n>50){
+//     n=n/2;
+//     num=num+1;
+//     console.log(num, n)
+// }
